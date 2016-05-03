6dd6fcea1c748859ba50fd8549fbe041d408c9d8
Guillaume <guillaume.viejo@gmail.com>
2016-05-03 13:24:19 +0200

    transfering

--- run/sferes/test.py
+++ run/sferes/test.py
@@ -10,39 +10,29 @@ from Models import *
 from Sferes import pareto
 import cPickle as pickle
 
-with open("SFERES_1_best_parameters.pickle", 'rb') as f:
+with open("SFERES_1_only_fusion_best_parameters.pickle", 'rb') as f:
 	data = pickle.load(f)
 
 
-
 front = pareto()
-
+parameters = data['tche']['m']['fusion']
+parameters['length'] = 3.0
+parameters['gain'] = 1.0
+parameters['sigma'] = 0.001
+print parameters
+model = FSelection()
+fit = model.sferes_call(front.monkeys['m'], front.rt_reg_monkeys['m'], parameters)
+print fit[0], fit[1]
 
 figure()
 for i in xrange(1, 6):
 	subplot(2,3,i)
 	index = np.where(front.rt_reg_monkeys['m'][:,0] == i)[0]
-	plot(np.arange(0,i), front.rt_reg_monkeys['m'][index[0:i],1], 'o-')
-	plot(np.arange(i, i+len(front.rt_reg_monkeys['m'][index[i:],1])), front.rt_reg_monkeys['m'][index[i:],1], '*-')
-show()
+	plot(np.arange(0,i), front.rt_reg_monkeys['m'][index[0:i],1], 'o-', color = 'black')
+	plot(np.arange(i, i+len(front.rt_reg_monkeys['m'][index[i:],1])), front.rt_reg_monkeys['m'][index[i:],1], '*-', color='black')
 
+	plot(np.arange(0,i), model.rt_model[index[0:i]], 'o-')
+	plot(np.arange(i, i+len(model.rt_model[index[i:]])), model.rt_model[index[i:]], '*-')
 
-
-sys.exit()
-parameters = data['tche']['m']['fusion']
-parameters['gain'] = 1.0
-
-model = FSelection()
-
-fit = model.sferes_call(front.monkeys['m'], front.rt_reg_monkeys['m'], parameters)
-
-print fit[0], fit[1]
-
-figure()
-
-for i in xrange(1, 6):	
-	subplot(2,3,i)
-	plot(model.rt_model[model.mean_rt[:,0] == i], 'o-')
-	plot(model.mean_rt[model.mean_rt[:,0] == i,1], '*--', color = 'black')
 
 show()
